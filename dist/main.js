0480f69b77a8ccd7bcd12c84090d951c4a2adf3d
Luciano Di Ricci <lucianodiricci@gmail.com>
2022-07-28 18:16:17 -0300

    js logic started

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,25 @@
 /******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
+/***/ (() => {
 
-console.log("The Maestruli sends his regards.");
+throw new Error("Module parse failed: Identifier 'title' has already been declared (5:8)\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\n| //code\n| const todoListFactory = (title, description, checkList, dueDate, priority, isComplete) => {\n>     let title = title;\n|     let description = description;\n|     let dueDate = dueDate;");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFDQSxnRCIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tdG9kby1saXN0Ly4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIlxuY29uc29sZS5sb2coXCJUaGUgTWFlc3RydWxpIHNlbmRzIGhpcyByZWdhcmRzLlwiKTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7O1VDQUE7VUFDQTtVQUNBO1VBQ0E7VUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL29kaW4tdG9kby1saXN0L3dlYnBhY2svYmVmb3JlLXN0YXJ0dXAiLCJ3ZWJwYWNrOi8vb2Rpbi10b2RvLWxpc3Qvd2VicGFjay9zdGFydHVwIiwid2VicGFjazovL29kaW4tdG9kby1saXN0L3dlYnBhY2svYWZ0ZXItc3RhcnR1cCJdLCJzb3VyY2VzQ29udGVudCI6WyIiLCIvLyBzdGFydHVwXG4vLyBMb2FkIGVudHJ5IG1vZHVsZSBhbmQgcmV0dXJuIGV4cG9ydHNcbi8vIFRoaXMgZW50cnkgbW9kdWxlIGRvZXNuJ3QgdGVsbCBhYm91dCBpdCdzIHRvcC1sZXZlbCBkZWNsYXJhdGlvbnMgc28gaXQgY2FuJ3QgYmUgaW5saW5lZFxudmFyIF9fd2VicGFja19leHBvcnRzX18gPSB7fTtcbl9fd2VicGFja19tb2R1bGVzX19bXCIuL3NyYy9pbmRleC5qc1wiXSgpO1xuIiwiIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
--- src/index.js
+++ src/index.js
@@ -1,2 +1,44 @@
-
+//test
 console.log("The Maestruli sends his regards.");
+//code
+
+const todoListFactory = (title, description, dueDate, priority, isComplete) => {
+    let checkList = [];
+    let setDate;
+    const addDueDate = (dueDate) =>{
+        let today = dateToday();
+        if (dueDate <= today){
+            //dont set?
+            alert("Your date is before today");
+        }
+        setDate = dueDate;
+    }
+
+    const addCheckItem = (item, isChecked){
+        checkList.push({item, isChecked});
+    }
+
+    
+    
+
+    
+    return{ title, description, checkList, setDate, priority, isComplete, addCheckItem}
+}
+
+const defaultList = todoListFactory( "Vacation", "What a time to be alive", ["bla","blabla","bla"], "01/01/2023", "red", false);
+console.table(defaultList);
+
+const projectFactory = (title, description) =>{
+
+}
+
+const dateToday = (){
+    let today = new Date();
+    let day = today.getDate();
+    let month = today.getMonth() + 1;
+    let year = today.getFullYear();
+
+    // This arrangement can be altered based on how we want the date's format to appear.
+    let currentDate = `${day}/${month}/${year}`;
+    return currentDate
+}
